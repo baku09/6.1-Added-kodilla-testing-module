71d5530621a84ec37569327daf2e5ca9c979e0dc
Jakub Bak <ktb.1999.ktb@gmail.com>
2023-11-08 20:44:24 +0100

    first commit

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Calculator/TestingMain.java
@@ -0,0 +1,14 @@
+package com.kodilla.testing.Calculator;
+
+public class TestingMain {
+    public static void main(String[] args) {
+
+
+        Calculator calculator = new Calculator();
+        int resultAdd = calculator.add(10, 22);
+        int resultSubstract = calculator.substract(44, 22);
+
+        System.out.printf("Wynik dodawania: " + resultAdd);
+        System.out.printf("\nWynik odejmowania: " + resultSubstract);
+    }
+}
