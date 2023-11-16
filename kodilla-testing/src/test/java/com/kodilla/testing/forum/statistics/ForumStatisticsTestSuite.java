27a70f41336f56f06fb515b3d58532c3c0d20c0c
Jakub Bak <ktb.1999.ktb@gmail.com>
2023-11-16 20:34:02 +0100

    6.7poprawione

--- kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/forum/statistics/ForumStatisticsTestSuite.java
@@ -2,6 +2,8 @@ package com.kodilla.testing.forum.statistics;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
 public class ForumStatisticsTestSuite {
@@ -16,59 +18,77 @@ public class ForumStatisticsTestSuite {
 
     @Test
     void testCalculateAdvStatisticsWithZeroPosts() {
+        // Given
         when(statisticsMock.postsCount()).thenReturn(0);
-
+        // When
         forumStatistics.calculateAdvStatistics(statisticsMock);
-        forumStatistics.showStatistics();
+        // Then
+        assertEquals(0, forumStatistics.getPostCount());
+        assertEquals(0, forumStatistics.getAveragePostsPerUser());
+        assertEquals(0, forumStatistics.getAverageCommentsPerPost());
     }
 
     @Test
     void testCalculateAdvStatisticsWithThousandPosts() {
+        // Given
         when(statisticsMock.postsCount()).thenReturn(1000);
-
+        // When
         forumStatistics.calculateAdvStatistics(statisticsMock);
-        forumStatistics.showStatistics();
+        // Then
+        assertEquals(1000, forumStatistics.getPostCount());
     }
 
     @Test
     void testCalculateAdvStatisticsWithZeroComments() {
+        // Given
         when(statisticsMock.commentsCount()).thenReturn(0);
-
+        // When
         forumStatistics.calculateAdvStatistics(statisticsMock);
-        forumStatistics.showStatistics();
+        // Then
+        assertEquals(0, forumStatistics.getCommentCount());
     }
 
     @Test
     void testCalculateAdvStatisticsWithCommentsLessThanPosts() {
+        // Given
         when(statisticsMock.postsCount()).thenReturn(5);
         when(statisticsMock.commentsCount()).thenReturn(3);
-
+        // When
         forumStatistics.calculateAdvStatistics(statisticsMock);
-        forumStatistics.showStatistics();
+        // Then
+        assertEquals(5, forumStatistics.getPostCount());
+        assertEquals(3, forumStatistics.getCommentCount());;
     }
 
     @Test
     void testCalculateAdvStatisticsWithCommentsMoreThanPosts() {
+        // Given
         when(statisticsMock.postsCount()).thenReturn(3);
         when(statisticsMock.commentsCount()).thenReturn(5);
-
+        // When
         forumStatistics.calculateAdvStatistics(statisticsMock);
-        forumStatistics.showStatistics();
+        // Then
+        assertEquals(3, forumStatistics.getPostCount());
+        assertEquals(5, forumStatistics.getCommentCount());
     }
 
     @Test
     void testCalculateAdvStatisticsWithZeroUsers() {
+        // Given
         when(statisticsMock.usersNames()).thenReturn(java.util.Collections.emptyList());
-
+        // When
         forumStatistics.calculateAdvStatistics(statisticsMock);
-        forumStatistics.showStatistics();
+        // Then
+        assertEquals(0, forumStatistics.getUserCount());
     }
 
     @Test
     void testCalculateAdvStatisticsWithHundredUsers() {
+        // Given
         when(statisticsMock.usersNames()).thenReturn(java.util.Collections.nCopies(100, "User"));
-
+        // When
         forumStatistics.calculateAdvStatistics(statisticsMock);
-        forumStatistics.showStatistics();
+        // Then
+        assertEquals(100, forumStatistics.getUserCount());
     }
 }
