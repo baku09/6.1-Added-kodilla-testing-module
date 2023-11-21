c2d78548f6b4a915340aade8d85859529818153d
Jakub Bak <ktb.1999.ktb@gmail.com>
2023-11-21 20:07:40 +0100

    7.5poprawione

--- kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/array/ArrayOperations.java
@@ -3,15 +3,17 @@ package com.kodilla.stream.array;
 import java.util.stream.IntStream;
 
 public interface ArrayOperations {
-     static double getAverage(int[] numbers){
-         System.out.println("Array elements:");
-         IntStream.range(0, numbers.length)
-                 .forEach(i -> {
-                     System.out.println(numbers[i] + " ");
-                 });
-         System.out.println("Calculate average:");
-         return IntStream.range(0, numbers.length)
-                 .average()
-                 .orElse(0);
-     }
+    static double getAverage(int[] numbers) {
+        System.out.println("Elements of the array:");
+        IntStream.range(0, numbers.length)
+                .forEach(i -> System.out.print(numbers[i] + " "));
+
+        double average = IntStream.of(numbers)
+                .average()
+                .orElse(0.0);
+
+        System.out.println("\nAverage: " + average);
+        return average;
+    }
 }
+
--- kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
+++ kodilla-stream/src/test/java/com/kodilla/stream/array/ArrayOperationsTestSuite.java
@@ -11,9 +11,9 @@ public class ArrayOperationsTestSuite {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20};
 
         // When
-        double average = ArrayOperations.getAverage(numbers);
+        double result = ArrayOperations.getAverage(numbers);
 
         // Then
-        assertEquals(10.5, average);
+        assertEquals(10.5, result);
     }
 }
