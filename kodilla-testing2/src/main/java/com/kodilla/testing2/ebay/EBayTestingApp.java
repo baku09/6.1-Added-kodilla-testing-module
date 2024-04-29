9bc071c91ac13008c3a42fe3e274d0cde13fb154
Jakub Bak <ktb.1999.ktb@gmail.com>
2024-04-29 18:30:28 +0200

    29.1

---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/ebay/EBayTestingApp.java
@@ -0,0 +1,22 @@
+package com.kodilla.testing2.ebay;
+
+import com.kodilla.testing2.google.config.WebDriverConfig;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+
+public class EBayTestingApp {
+    public static void main(String[] args) {
+        WebDriver driver = WebDriverConfig.getDriver(WebDriverConfig.CHROME);
+
+        // Otwieranie strony eBay
+        driver.get("https://www.ebay.com/");
+
+        // Wyszukiwanie laptopów
+        driver.findElement(By.id("gh-ac")).sendKeys("Laptop");
+        driver.findElement(By.id("gh-ac")).sendKeys(Keys.RETURN);
+
+        // Zamykanie przeglądarki
+        //driver.quit();
+    }
+}
